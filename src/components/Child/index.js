86f3b3598ca27d6c51c5222f37f8f6ea6f19287c
Bryan Emerson <bryan.l.emerson@gmail.com>
2021-10-17 13:11:40 -0400

    child component

---
+++ src/components/Child/index.js
@@ -0,0 +1,11 @@
+import './style.css'
+
+export default function Child() {
+  return (
+    <div className='child'>
+      <p>How many Scoops would you like brodie?</p>
+      <button>Clicky Up</button>
+      <button>Clicky Down</button>
+    </div>
+  )
+}
--- src/components/Parent/index.js
+++ src/components/Parent/index.js
@@ -1,9 +1,15 @@
+import {useState} from 'react'
+import Child from '../Child/index.js'
 import './style.css'
 
 export default function Parent() {
+let [count, setCount] = useState(10)
+
   return (
     <div className='parent'>
-      <h2>cool stuff fam</h2>
+      <h2>Ice Cream Scooper</h2>
+      <p>Num of Scoops: {count}</p>
+      <Child />
     </div>
   )
 }
