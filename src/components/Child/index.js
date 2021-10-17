17d35d6dc32a80ccf9d2092f1ce4d9bca2456780
Bryan Emerson <bryan.l.emerson@gmail.com>
2021-10-17 13:16:22 -0400

    props passed

--- src/components/Child/index.js
+++ src/components/Child/index.js
@@ -1,11 +1,11 @@
 import './style.css'
 
-export default function Child() {
+export default function Child(props) {
   return (
     <div className='child'>
       <p>How many Scoops would you like brodie?</p>
-      <button>Clicky Up</button>
-      <button>Clicky Down</button>
+      <button onClick={props.up}>Clicky Up</button>
+      <button onClick={props.down}>Clicky Down</button>
     </div>
   )
 }
--- src/components/Parent/index.js
+++ src/components/Parent/index.js
@@ -5,11 +5,19 @@ import './style.css'
 export default function Parent() {
 let [count, setCount] = useState(10)
 
+let decrement = ()=> {
+  setCount(count - 1)
+}
+
+let increment = ()=> {
+  setCount(count + 1)
+}
+
   return (
     <div className='parent'>
       <h2>Ice Cream Scooper</h2>
       <p>Num of Scoops: {count}</p>
-      <Child />
+      <Child down={decrement} up={increment}/>
     </div>
   )
 }
